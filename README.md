af82abb5f55688da4b8fd75b9018df140263e30b
pwnjack <pwnjack@gmail.com>
2016-11-30 18:05:23 +0100

    First Commit

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# themecraft
+# Themecraft 2
+
+Based off Underscores (_S), is a Wordpress development starting point.
---
+++ inc/jetpack.php
@@ -0,0 +1,41 @@
+<?php
+/**
+ * Jetpack Compatibility File.
+ *
+ * @link https://jetpack.com/
+ *
+ * @package Themecraft
+ */
+
+/**
+ * Jetpack setup function.
+ *
+ * See: https://jetpack.com/support/infinite-scroll/
+ * See: https://jetpack.com/support/responsive-videos/
+ */
+function themecraft_jetpack_setup() {
+	// Add theme support for Infinite Scroll.
+	add_theme_support( 'infinite-scroll', array(
+		'container' => 'main',
+		'render'    => 'themecraft_infinite_scroll_render',
+		'footer'    => 'page',
+	) );
+
+	// Add theme support for Responsive Videos.
+	add_theme_support( 'jetpack-responsive-videos' );
+}
+add_action( 'after_setup_theme', 'themecraft_jetpack_setup' );
+
+/**
+ * Custom render function for Infinite Scroll.
+ */
+function themecraft_infinite_scroll_render() {
+	while ( have_posts() ) {
+		the_post();
+		if ( is_search() ) :
+			get_template_part( 'template-parts/content', 'search' );
+		else :
+			get_template_part( 'template-parts/content', get_post_format() );
+		endif;
+	}
+}
